e888c89e6e54621c902416d3bba3825456c0293a
Irina <muxamedzuanoa@mail.com>
2023-02-27 22:48:42 +0500

    Задание 9

---
+++ OneDrive/Рабочий стол/Семинар 2 С#/task09/Program.cs
@@ -0,0 +1,22 @@
+﻿// 9. Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+
+// 78 -> 8
+// 12-> 2
+
+ 
+int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+Console.WriteLine($"Наибольшая цифра числа -> {result}"); 
+
+int maxDigit1 = MaxDigit(45);
+Console.WriteLine($"Наибольшая цифра числа -> {maxDigit1}");
+
+int MaxDigit(int num) // num = number
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return result;
+}
+
