8103d2b217c6888b13149da37b36ed97d3733785
Irina <muxamedzuanoa@mail.com>
2023-03-06 17:19:53 +0500

    Added file .gitignore

---
+++ OneDrive/Рабочий стол/Учёба/Знакомство с  языками программирования/Семинар 2 С# д.з/task15/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите номер для недели:");
+int number = Convert.ToInt32(Console.ReadLine());
+bool checkNumber = CheckNumber(number);
+Console.WriteLine(checkNumber == true ? "Да" : "Нет");
+
+bool CheckNumber(int num)
+{
+    return num == 6 || num == 7;
+}
