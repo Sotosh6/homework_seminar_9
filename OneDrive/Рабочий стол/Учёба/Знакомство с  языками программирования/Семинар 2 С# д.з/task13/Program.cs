91d80bcc41a667de466ffb592c8fb6f2aa4725c9
Irina <muxamedzuanoa@mail.com>
2023-03-06 18:13:43 +0500

    Added task 13

--- OneDrive/Рабочий стол/Учёба/Знакомство с  языками программирования/Семинар 2 С# д.з/task13/Program.cs
+++ OneDrive/Рабочий стол/Учёба/Знакомство с  языками программирования/Семинар 2 С# д.з/task13/Program.cs
@@ -5,3 +5,18 @@
 // 78 -> третьей цифры нет
 
 // 32679 -> 6
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
+else 
+{
+    while (number > 999)
+        {
+            number = number / 10;
+        }
+    number = number - (number / 10) * 10;
+    Console.WriteLine(number);
+}    
