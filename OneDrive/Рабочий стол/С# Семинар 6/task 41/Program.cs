d9f6fc16fe93883dfce6f155f91b703ebcf3caed
Irina <muxamedzuanoa@mail.com>
2023-04-03 19:45:00 +0500

    task 41

---
+++ OneDrive/Рабочий стол/С# Семинар 6/task 41/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// -1, -7, 567, 89, 223-> 3
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int [] InputArray(int lenght)
+{
+    int[] arr = new int[lenght];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Prompt($"Введите {i + 1}-й элемент: ");
+    }
+    return arr;
+}
+
+void PrintArray(int [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"a[{i}] = {arr[i]}");
+    }
+}
+
+int CountPositiveNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int lenght = Prompt("Введите количество элементов: ");
+int[] arr;
+arr = InputArray(lenght);
+PrintArray(arr);
+Console.WriteLine($"Количество чисел больше 0: {CountPositiveNumbers(arr)}");
